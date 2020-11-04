32372425e37fa57e178f7b9841f34c72eb61ed8d
JIE SHI <20091665@mail.wit.ie>
2020-11-04 13:49:53 +0000

    Movie Details Page - included production countries

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -51,6 +51,16 @@ export default ({ movie }) => {
           </li>
         ))}
       </ul>
+      <ul className="list-group list-group-horizontal">
+        <li key="pch" className="list-group-item list-group-item-dark">
+          Production Countries
+        </li>
+        {movie.production_countries.map(coun => (
+          <li key={coun.name} className="list-group-item">
+            {coun.name}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
