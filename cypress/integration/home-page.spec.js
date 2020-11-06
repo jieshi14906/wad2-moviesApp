19299851cc658ad6ed43c6aaee26e20fc9e2505b
JIE SHI <20091665@mail.wit.ie>
2020-11-06 13:11:09 +0000

    E2E tests - Home page - Filtering by genre

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -54,9 +54,27 @@ describe("Home Page ", () => {
               .should("have.text", matchingMovies[index].title);
             })
           })
+          it("should display movies with 'xyz' in the title", () => {
+            const searchString = "xyz";
+            const matchingMovies = filterByTitle(movies, searchString);
+            cy.get("input").clear().type(searchString);
+            cy.get(".card").should("have.length", 0);
+          })
         })
         describe("By movie genre" ,() => {
-          // More later
+            it("should display movies with the specified genre only", () => {
+                const selectedGenreId = 35;
+                const selectedGenreText = "Comedy";
+                const matchingMovies = filterByGenre(movies, selectedGenreId);
+                cy.get("select").select(selectedGenreText); 
+                cy.get(".card").should("have.length", matchingMovies.length);
+                cy.get(".card").each(($card, index) => {
+                  cy.wrap($card)
+                    .find(".card-title")
+                    .should("have.text", matchingMovies[index].title);
+                });      
+              });
+          });
+        
+        });// More later
         })
-      })
-  })
