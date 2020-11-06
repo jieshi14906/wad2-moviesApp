17935162ae22c8a0f9d3c183af66263cef35f617
JIE SHI <20091665@mail.wit.ie>
2020-11-06 14:46:43 +0000

    E2E tests - Navigation - Movies Details page

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -48,4 +48,20 @@ describe("Navigation", () => {
     });
   });
 
+  describe("From the Movie Details page ", () => {
+    beforeEach(() => {
+      cy.visit(`/movies/${movieId}`);
+    });
+    it("should change browser URL when show/hide reviews is clicked", () => {
+      cy.contains("Show Reviews").click();
+      cy.url().should("include", `/movies/${movieId}/reviews`);
+      cy.contains("Hide Reviews").click();
+      cy.url().should("not.include", `/movies/${movieId}/reviews`);
+    });
+    it("navigate to the full review page when a 'Full Review' link is clicked", () => {
+        cy.contains("Show Reviews").click();
+        cy.contains("Full Review").click();
+        cy.url().should("include", `/reviews/${reviews[0].id}`);
+    });
+  });
 });
