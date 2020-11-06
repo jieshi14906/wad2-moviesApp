64bcb45edfb17232029b1789724e839c30b34da2
JIE SHI <20091665@mail.wit.ie>
2020-11-06 14:47:57 +0000

    E2E tests - Navigation - Favorites page

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -64,4 +64,16 @@ describe("Navigation", () => {
         cy.url().should("include", `/reviews/${reviews[0].id}`);
     });
   });
+  describe("From the Favorites page", () => {
+    beforeEach(() => {
+      cy.visit("/");
+      cy.get(".card").eq(0).find("button").click();
+      cy.get("nav").find("li").eq(2).find("a").click();
+    });
+    it("should navigate to the movies detail page and change the browser URL", () => {
+      cy.get(".card").eq(0).find("img").click();
+      cy.url().should("include", `/movies/${movies[0].id}`);
+      cy.get("h2").contains(movies[0].title);
+    });
+  });
 });
