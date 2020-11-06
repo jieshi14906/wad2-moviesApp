6404bb52391aae389707b1aa5726a893899697d4
JIE SHI <20091665@mail.wit.ie>
2020-11-06 13:59:11 +0000

    E2E tests - Movie Details page - poster image

--- cypress/integration/movieDetails-page.spec.js
+++ cypress/integration/movieDetails-page.spec.js
@@ -52,4 +52,9 @@ describe("Movie Details Page", () => {
       .should("have.attr", "href")
       .should("include", movie.homepage);
   });
+    it("should display the movie poster", () => {
+    cy.get("img")
+      .should("have.attr", "src")
+      .should("include",movie.poster_path);
+  });
 });
