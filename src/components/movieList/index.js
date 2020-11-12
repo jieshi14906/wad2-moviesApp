e5d0c59c33b2cbfe5fa9a9ca1b3bb6c6f2ed3f01
JIE SHI <20091665@mail.wit.ie>
2020-11-12 14:49:27 +0000

    Fix buttons on movie list pages

--- src/components/movieList/index.js
+++ src/components/movieList/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import Movie from "../movieCard/";
 import "./movieList.css";
 
-const MovieList = props => {
-  const movieCards = props.movies.map(m => (
-    <Movie key={m.id} movie={m} buttonHandler={props.buttonHandler} />
-    ));
-    return <div className="row movies bg-info">{movieCards}</div>;
-  };
+const MovieList = ({movies, action}) => {
+  const movieCards = movies.map(m => (
+    <Movie key={m.id} movie={m} action={action} />
+  ));
+  return <div className="row movies bg-info">{movieCards}</div>;
+};
 
 export default MovieList;
