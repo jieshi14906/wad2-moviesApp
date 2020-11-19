4eae1f2baf16d9ad6e6baf967a0740fa842af872
JIE SHI <20091665@mail.wit.ie>
2020-11-19 21:00:27 +0000

    CI - Add test stage to pipeline

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -11,7 +11,7 @@ const MovieListPage = () => {
 
   return (
       <PageTemplate 
-        title='No. Movies'
+        title='Discover Movies'
         movies={movies}
         action={(movie) => {
           return <AddToFavoritesButton movie={movie} /> 
